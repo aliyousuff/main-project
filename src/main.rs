15e4cd928d64ee0f69fd75c3431834b99ed5abf2
Yousuf Ali <aliyyousuf@gmail.com>
2025-05-03 23:29:37 +0600

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use math;
 
-fn main() {
+fn main() 
+{
     println!("From the math submodule: {}", math::add(20, 20));
     println!("From the math submodule's divide: {:?}", math::divide(10, 3));
     println!("From the math submodule's subtract: {}", math::subtract(10, 3));
