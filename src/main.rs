9fcee858ebd66d1d67967b5c0b9956d3ad6e63f1
Yousuf Ali <aliyyousuf@gmail.com>
2025-05-04 00:02:31 +0600

    Updated submodule & running the multiplication function from the main-project

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use math;
 
 fn main() 
 {
-    println!("From the math submodule: {}", math::add(20, 20));
+    println!("From the math addition submodule: {}", math::add(20, 20));
     println!("From the math submodule's divide: {:?}", math::divide(10, 3));
     println!("From the math submodule's subtract: {}", math::subtract(10, 3));
+    println!("From the math submodule's multiplication: {}", math::multiplication(10, 3));
+
 
 }
