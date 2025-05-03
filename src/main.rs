180fb277b6309406bf884225e01c5d99bf50fb1a
Yousuf Ali <aliyyousuf@gmail.com>
2025-05-03 23:22:34 +0600

    Running the submodule's subtract function from the main-project

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,6 @@ use math;
 fn main() {
     println!("From the math submodule: {}", math::add(20, 20));
     println!("From the math submodule's divide: {:?}", math::divide(10, 3));
+    println!("From the math submodule's subtract: {}", math::subtract(10, 3));
+
 }
